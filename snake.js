fab3b0a6aabc2e613167b7679328dff93e8aaf49
FeliCaldas <150372987+FeliCaldas@users.noreply.github.com>
2024-08-20 17:30:34 -0300

    g

---
+++ snake.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+const gridSize = 20;
+const canvasSize = 400;
+
+let snake = [{ x: gridSize * 5, y: gridSize * 5}];
+let direction = { x: 1, y: 0 };
+
+let food = { x: gridSize * 10, y: gridSize * 10};
+
+let gameInterval;
